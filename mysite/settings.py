0431871bc7fc3fbca933e020ff52f9169fade14f
eunag63 <kibm63@naver.com>
2021-05-21 00:33:03 +0900

    파일 사용하기

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bs!_j#9)u5tqok=4s3bjjv$d96*d$v-3p#n52wa1il%wu!!9z7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost']
 

5f3150241a539974d33d10c3c2688b0d095325e7
eunag63 <kibm63@naver.com>
2021-05-21 00:12:26 +0900

    네비게이션바 추가, 첫화면으로 가기

--- electione/urls.py
+++ electione/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 #앱 접속시 electione에서 접속해라
+app_name = 'electione'
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='home'),
     path('areas/<str:area>/', views.areas),
     path('polls/<int:poll_id>', views.polls),
     path('areas/<str:area>/results/', views.results),
